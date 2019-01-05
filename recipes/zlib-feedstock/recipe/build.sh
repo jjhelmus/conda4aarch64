a407ec2eacf75d15fda8cb8a54f1c8f0f0d0f3c3
Jonathan Helmus <jjhelmus@gmail.com>
2019-01-05 22:21:17 +0000

    zlib recipe

---
+++ recipes/zlib-feedstock/recipe/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./configure --prefix=${PREFIX}  \
+            --shared
+
+make -j${CPU_COUNT} ${VERBOSE_AT}
+make check
+make install
+
+# Remove man files.
+rm -rf $PREFIX/share
+
+# Copy license file to the source directory so conda-build can find it.
+cp $RECIPE_DIR/license.txt $SRC_DIR/license.txt
